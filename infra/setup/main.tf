942463f8fc8cf738ca044522ee5d5af47ae247b1
Jordi Magaña Franco <jokermak0173@gmail.com>
2025-01-06 20:04:23 -0600

    Configure Terraform

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "joker-devops-recipe-app-tf-state"
+    key            = "tf-state-setup"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
