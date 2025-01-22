7f95096ff2790b6a98a5fc0dce04ebf629b4545b
Jordi Magaña Franco <jokermak0173@gmail.com>
2025-01-21 18:49:08 -0600

    Added EFS File System

---
+++ infra/deploy/efs.tf
@@ -0,0 +1,10 @@
+###########################
+# EFS form media storage. #
+###########################
+
+resource "aws_efs_file_system" "media" {
+  encrypted = true
+  tags = {
+    Name = "${local.prefix}-media"
+  }
+}
